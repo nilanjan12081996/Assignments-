8c4e898577bc1fd78647a3ad851c649dacf9a4b8
nilanjan12081996 <107671025+nilanjan12081996@users.noreply.github.com>
2022-09-08 13:35:02 +0530

    Create Question.java

---
+++ my_assignment/quiz/Question.java
@@ -0,0 +1,17 @@
+/*
+ * @Author Nilanjan Dasgupta
+ */
+package quiz;
+//create a question class and declare 2 instance variable to store question and answers
+public class Question {
+	String question;
+	String ans;
+	//constructor for pass the question and ans
+	public Question(String question, String ans) {
+		super();
+		this.question = question;
+		this.ans = ans;
+	}
+	
+
+}
